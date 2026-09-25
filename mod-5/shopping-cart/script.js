4055e9034b81c0ec208d81eac05a469aa9673624
jadedrb <jadecdrb@gmail.com>
2026-09-25 11:49:24 -0400

    render cart

--- mod-5/shopping-cart/script.js
+++ mod-5/shopping-cart/script.js
@@ -1,7 +1,7 @@
 // TASK 1
 
 // 1. Create an empty array called shoppingList.
-let shoppingList = ['Apple', 'Orange', 'Banana'];
+let shoppingList = [];
 
 // 2. Write a function called addItem that takes an item as a parameter and adds it to the shoppingList array.
 // function addItem(item) {
@@ -72,5 +72,53 @@ function filterItems(searchTerm) {
 
 // TASK 3
 
-// 2. Write a JavaScript function that adds items to the array and updates the displayed list dynamically when the button is clicked.
+// 2. Write a JavaScript function that adds items to the array and updates the displayed list 
+// dynamically when the button is clicked.
+
+const button = document.getElementById("add");
+const input = document.getElementById("item");
+const ul = document.querySelector('ul');
+
+function handleClick() {
+
+    // access the input value to get the item
+    const item = input.value;
+
+    // add the item to the shoppingList array
+    addItem(item);
+  
+    // create the output based off the array
+    renderCart()
+   
+    // reset the input (so they can type a new item more easily)
+    input.value = '';
+
+    // focus on the input (so they don't have to click it again before typing)
+    input.focus();
+}
+
+// make the button clickable
+button.addEventListener('click', handleClick);
+
+// create the output based off the data in shopping cart array
+function renderCart() {
+
+    // start with a clean slate (the <ul></ul> should be empty)
+    ul.innerHTML = '';
+
+    // loop over the shopping cart and turn each item into a <li></li>
+    for (let item of shoppingList) {
+        // create the output
+        const li = document.createElement('li');
+
+        // give our list item some text (the item's name)
+        li.textContent = item;
+
+        // add it to the DOM (appending it to the <ul></ul>)
+        ul.appendChild(li);
+    }
+
+}
+
 // 3. Write another function that removes the last item and updates the displayed list when a “Remove Last Item” button is clicked.
+
