7f49602b6a617b3b298f016d41adcd7ee06297ed
jadedrb <jadecdrb@gmail.com>
2026-09-25 11:00:56 -0400

    filter items

--- mod-5/shopping-cart/script.js
+++ mod-5/shopping-cart/script.js
@@ -4,12 +4,10 @@
 let shoppingList = ['Apple', 'Orange', 'Banana'];
 
 // 2. Write a function called addItem that takes an item as a parameter and adds it to the shoppingList array.
-function addItem(item) {
-    // add the item to the end of the array (if it's not already in the list)
-    if (!shoppingList.includes(item)) {
-        shoppingList.push(item);
-    }
-}
+// function addItem(item) {
+//     // add the item to the end of the array 
+//     shoppingList.push(item);
+// }
 
 // 3. Write a function called removeLastItem that removes the last item from the shoppingList array.
 function removeLastItem() {
@@ -27,5 +25,43 @@ function displayList() {
 // TASK 2
 
 // 1. Modify the addItem function to only add the item if it is not already in the shoppingList array.
+function addItem(item) {
+    // add the item to the end of the array (if it's not already in the list)
+    if (!shoppingList.includes(item)) {
+        shoppingList.push(item);
+    }
+}
 
-// 2. Write a function called filterItems that takes a search term as a parameter and returns all items in the shoppingList that contain that search term (case-insensitive).
+// function isItemInArray(item) {
+
+//     // for (let i = 0; i < shoppingList.length; i++) {
+//     //     let item = shoppingList[i];
+//     // }
+
+//     for (let itemInCart of shoppingList) {
+//         if (itemInCart === item) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// 2. Write a function called filterItems that takes a search term as a parameter and 
+// returns all items in the shoppingList that contain that search term (case-insensitive).
+function filterItems(searchTerm) {
+
+    // keep track of all items that match the searchTerm
+    let filter = [];
+
+    for (let item of shoppingList) {
+
+        // check if the item contains the searchTerm
+        if (item.toLowerCase().includes(searchTerm.toLowerCase())) { 
+            filter.push(item);
+        }
+    }
+
+    return filter;
+}
+
