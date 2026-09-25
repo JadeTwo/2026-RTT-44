1c76871e1af17ef32af5d20a5afa1b628ebc4874
jadedrb <jadecdrb@gmail.com>
2026-09-25 14:04:25 -0400

    old script with original lab code

--- mod-5/shopping-cart/script.js
+++ mod-5/shopping-cart/script.js
@@ -1,68 +1,49 @@
-// TASK 1
+// select elements
+const button = document.getElementById("add");
+const input = document.getElementById("item");
+const ul = document.querySelector('ul');
+const removeBtn = document.getElementById('remove');
 
-// 1. Create an empty array called shoppingList.
+// an empty array called shoppingList.
 let shoppingList = [];
 
-// 2. Write a function called addItem that takes an item as a parameter and adds it to the shoppingList array.
-// function addItem(item) {
-//     // add the item to the end of the array 
-//     shoppingList.push(item);
-// }
 
-// 3. Write a function called removeLastItem that removes the last item from the shoppingList array.
+// a function called removeLastItem that removes the last item from the shoppingList array.
 function removeLastItem() {
     // remove the last item of the array
     shoppingList.pop();
 }
 
-// 4. Write a function called displayList that logs all items in the shoppingList array to the console.
+// afunction called displayList that logs all items in the shoppingList array to the console.
 function displayList() {
     // log all items
     console.log(shoppingList);
 }
 
-
-// TASK 2
-
-// 1. Modify the addItem function to only add the item if it is not already in the shoppingList array.
+// addItem function to only add the item if it is not already in the shoppingList array.
 function addItem(item) {
-    // add the item to the end of the array (if it's not already in the list)
-    if (!shoppingList.includes(item)) {
+
+    // if filter items returns an empty array, the item hasn't been added yet
+    let itemsFound = filterItems(item.name)
+    
+    // check if any items with the given name were found
+    if (itemsFound.length === 0) {
         shoppingList.push(item);
     }
-    // an alternative would be to use the filterItems function
+    console.log(shoppingList);
 }
 
-// function isItemInArray(item) {
-
-//     // for (let i = 0; i < shoppingList.length; i++) {
-//     //     let item = shoppingList[i];
-//     // }
-
-//     for (let itemInCart of shoppingList) {
-//         if (itemInCart === item) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-
-// 2. Write a function called filterItems that takes a search term as a parameter and 
+// a function called filterItems that takes a search term as a parameter and 
 // returns all items in the shoppingList that contain that search term (case-insensitive).
 function filterItems(searchTerm) {
 
     // keep track of all items that match the searchTerm
     let filter = [];
 
+    // item is an object (with name, price, quantity)
     for (let item of shoppingList) {
-
-        // let lowerCaseItem = item.toLowerCase()
-        // let lowerCaseSearchTerm = searchTerm.toLowerCase()
-
         // check if the item contains the searchTerm
-        if (item.toLowerCase().includes(searchTerm.toLowerCase())) { 
-        // if (lowerCaseItem.includes(lowerCaseSearchTerm)) {
+        if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) { 
             filter.push(item);
         }
     }
@@ -70,20 +51,58 @@ function filterItems(searchTerm) {
     return filter;
 }
 
+// create the output based off the data in shopping cart array
+function renderCart() {
 
-// TASK 3
+    // start with a clean slate (the <ul></ul> should be empty)
+    ul.innerHTML = '';
 
-// 2. Write a JavaScript function that adds items to the array and updates the displayed list 
+    // loop over the shopping cart and turn each item into a <li></li>
+    for (let item of shoppingList) {
+
+        // create the output
+        const li = document.createElement('li');
+        const nameDiv = document.createElement('div');
+        const priceDiv = document.createElement('div');
+        const quantityInput = document.createElement('input');
+        const removeBtn = document.createElement('button');
+
+        // adding text to our elements here
+        nameDiv.textContent = item.name;
+        priceDiv.textContent = item.price;
+
+        // <li>
+        //     <div>apple</div>
+        //     <div>5</div>
+        //     <input type="number" value="1">
+        //     <button>X</button>
+        // </li>
+
+
+        // // give our list item some text (the item's name)
+        // li.textContent = `${item.name}, ${item.price}, ${item.quantity}`;
+
+
+        // add it to the DOM (appending it to the <ul></ul>)
+        ul.appendChild(li);
+    }
+
+}
+
+// a JavaScript function that adds items to the array and updates the displayed list 
 // dynamically when the button is clicked.
-
-const button = document.getElementById("add");
-const input = document.getElementById("item");
-const ul = document.querySelector('ul');
 
 function handleClick() {
 
-    // access the input value to get the item
-    const item = input.value;
+    // access the input value to get the item name
+    const itemName = input.value;
+
+    // create an item object
+    const item = {
+        name: itemName,
+        price: 5,
+        quantity: 1
+    };
 
     // add the item to the shoppingList array
     addItem(item);
@@ -101,31 +120,7 @@ function handleClick() {
 // make the button clickable
 button.addEventListener('click', handleClick);
 
-// create the output based off the data in shopping cart array
-function renderCart() {
-
-    // start with a clean slate (the <ul></ul> should be empty)
-    ul.innerHTML = '';
-
-    // loop over the shopping cart and turn each item into a <li></li>
-    for (let item of shoppingList) {
-        // create the output
-        const li = document.createElement('li');
-
-        // give our list item some text (the item's name)
-        li.textContent = item;
-
-        // add it to the DOM (appending it to the <ul></ul>)
-        ul.appendChild(li);
-    }
-
-}
-
-// 3. Write another function that removes the last item and updates the displayed 
-// list when a “Remove Last Item” button is clicked.
-
-const removeBtn = document.getElementById('remove');
-
+// another function that removes the last item and updates the displayed 
 removeBtn.addEventListener('click', function() {
     // remove the last item (from the array)
     removeLastItem();
